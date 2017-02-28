db18cc635d55704086f96f31240d78fa028074d8
Touqeer Ahmad <tqr093@gmail.com>
2017-02-28 15:27:00 +0500

    validation on user model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: { message: "Name is required" }, length: {in: 3..150}, numericality: false
+  validates :email, presence: { message: "Email is required"}, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :password, presence: true,  length: {in: 6..30},  confirmation: true
+  validates :cell, presence: true, length: {in: 7..14}, uniqueness: true
+  validates :verified, inclusion: {in: [true, false]}
+  validates :verified_token, uniqueness: true
+  validates :role, presence: true, acceptance: {accept: ["consumer", "driver", "admin"]}
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 User.delete_all
 
 User.create!([
-  {id: 1, name:"Touqeer", email:"touqeer@gmail.com", password:"123456", cell:"+923001234567", verified: false, verified_token: nil, role:"Consumer"},
-  {id: 2, name:"Ahmad", email:"ahmad@gmail.com", password:"123456", cell:"+923111234567", verified: false, verified_token: nil, role:"Consumer"},
-  {id: 3, name:"Ali", email:"ali@gmail.com", password:"123456", cell:"+923221234567", verified: false, verified_token: nil, role:"Consumer"},
-  {id: 4, name:"Zeshan", email:"zeshan@gmail.com", password:"123456", cell:"+923331234567", verified: false, verified_token: nil, role:"Consumer"},
-  {id: 5, name:"Admin1", email:"admin1@gmail.com", password:"123456", cell:"+923441234567", verified: true, verified_token: nil, role:"Admin"}
+  {id: 1, name:"Touqeer", email:"touqeer@gmail.com", password:"123456", cell:"+923001234567", verified: false, verified_token: nil, role:"consumer"},
+  {id: 2, name:"Ahmad", email:"ahmad@gmail.com", password:"123456", cell:"+923111234567", verified: false, verified_token: nil, role:"consumer"},
+  {id: 3, name:"Ali", email:"ali@gmail.com", password:"123456", cell:"+923221234567", verified: false, verified_token: nil, role:"consumer"},
+  {id: 4, name:"Zeshan", email:"zeshan@gmail.com", password:"123456", cell:"+923331234567", verified: false, verified_token: nil, role:"consumer"},
+  {id: 5, name:"Admin1", email:"admin1@gmail.com", password:"123456", cell:"+923441234567", verified: true, verified_token: nil, role:"admin"}
 ])
