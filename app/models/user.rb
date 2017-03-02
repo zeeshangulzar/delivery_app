e0cd3ab582a063be4e2db94e56213f2d9f75ec2e
zeeshangulzar <xeeshangulzar@gmail.com>
2017-03-02 17:31:16 +0500

    User:Integrated Twillio SMS Service

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ActiveRecord::Base
   # For api authentication
   has_many :authentication_tokens
 
+  TWILLIO_SID    = 'AC969f17cb864ec269d068d4e4ff32c397'
+  TWILLIO_AUTH   = 'f9ca79e8e38852c26e106876338bda2b'
+  TWILLIO_NUMBER = '+14692086400'
+
   validates :name, presence: { message: "Name is required" }, length: {in: 3..150}, numericality: false
   validates :email, presence: { message: "Email is required"}
   validates :password, presence: true,  confirmation: true
@@ -16,5 +20,12 @@ class User < ActiveRecord::Base
   validates :verified, inclusion: {in: [true, false]}
   #validates :verified_token, uniqueness: true
   validates :role, presence: true, inclusion: { in: ["consumer", "driver", "admin"] }
+
+  def send_sms
+    client = Twilio::REST::Client.new(TWILLIO_SID, TWILLIO_AUTH)
+    client.messages.create to: cell,
+    from: TWILLIO_NUMBER,
+    body: "ANA PIN: #{verified_token}"
+  end
 
 end
