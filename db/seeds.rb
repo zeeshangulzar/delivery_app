8488080d09bc8723ad1a17360cac387b63976db0
zeeshangulzar <xeeshangulzar@gmail.com>
2017-03-06 16:34:27 +0500

    Seed: Updated admin email

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-User.create!(id: 1, name: 'Admin', email: 'admin1@ana.com', password: '@n@admin', cell: '+923441234567', verified: true, role: 'admin' )
+User.create!(id: 1, name: 'Admin', email: 'admin@ana.com', password: '@n@admin', cell: '+923441234567', verified: true, role: 'admin' )
 
