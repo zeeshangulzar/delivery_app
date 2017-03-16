51b90871ec579b2c06c9fd3e30e55fff3212baf3
Touqeer Ahmad <touqeer.ahmed@novatoresols.com>
2017-03-16 18:07:44 +0500

    ANA-90: Map route defined

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,3 @@
 class HomeController < ApplicationController
 
-  def index
-    @locations=Location.all
-    @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
-      marker.lat location.lat
-      marker.lng location.lon
-      marker.infowindow location.name
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
   # For API through browser
   resources :users
 
+  get 'map' => 'map#map'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
