bccdff4e2760fb5f44ea63977381f321c9011a1f
zeeshangulzar <xeeshangulzar@gmail.com>
2017-03-21 16:19:14 +0500

    ANA_189: Added user profile UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   before_action :authenticate_user!
   before_action :validate_user_role, only: [:users_by_role]
+  before_action :set_user, only: [:show]
 
   def index
     render json: User.all
@@ -15,7 +16,14 @@ class UsersController < ApplicationController
     @users = User.get_list(params)
   end
 
+  def show
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def validate_user_role
     return redirect_to :root, notice: 'Please provide user role' if params[:role].blank?
