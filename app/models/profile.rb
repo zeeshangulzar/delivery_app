bff77e2cf5a98fab0f22adfbd673ab60d48acd30
Touqeer Ahmad <touqeer.ahmed@novatoresols.com>
2017-04-06 12:50:35 +0500

    Driver form changes updated

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,6 @@
 class Profile < ActiveRecord::Base
 
   belongs_to :user
-  validates :address, :plate_name, :license_number, :vehicle_model, :vehicle_type, :vehicle_make, presence: true
+  validates :address, :plate_name, :license_number, :vehicle_make, presence: true
 end
 
