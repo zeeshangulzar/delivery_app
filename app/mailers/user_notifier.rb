fe35c5454291519b4e4e8265b49cc8bac9bda279
Touqeer Ahmad <touqeer.ahmed@novatoresols.com>
2017-04-12 18:23:28 +0500

    ANA-220, ANA-223: No Product image attached

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,6 +1,11 @@
 class UserNotifier < ApplicationMailer
 
   default from: "noreply@ana.com"
+  before_action :set_image
+
+  def set_image
+    attachments.inline["no_product.jpg"] = File.read("#{Rails.root}/app/assets/images/no_product.jpg")
+  end
 
   def booking_email(booking)
     @booking = booking
